e47e617d5682658b483071b5798c42683e3dd5cc
vrunoa <vruno.alassia@gmail.com>
2016-03-02 20:14:08 -0300

    add implementation for linux and win os

--- cli.js
+++ cli.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 var child = require("child_process")
-var spawn = child.spawnSync("open", 
+var path = require('path')
+if(process.platform == "darwin") { 
+  var spawn = child.spawnSync("open", 
     ["-n", "-b", "com.microsoft.VSCode", "--args", process.cwd()
-])
+  ])
+}else if(process.platform == "linux") {
+  var open = path.join(__dirname, "xdg-open")
+  var spawn = child.spawnSync(open,  
+    ["-n", "-b", "com.microsoft.VSCode", "--args", process.cwd()
+  ])
+}else if(/win/.test(process.platform)) {
+  var spawn = child.spawnSync("cmd",
+    ["/c", "start", "-n", "-b", "com.microsoft.VSCode", "--args", process.cwd()
+  ])
+}
